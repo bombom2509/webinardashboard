a5056380262b8d4ca6f76f19648ba1cfca6fd094
Amartya Basu <amartyabasu2509@gmail.com>
2025-09-23 22:02:14 +0530

    Update dataviz2.py

--- dataviz2.py
+++ dataviz2.py
@@ -302,8 +302,8 @@ def create_full_report_pdf(df, logo_path, nursing_facilities_workforce, report_d
     return bytes(pdf.output())
 
 # --- Main App Logic ---
-file_location = r"D:\ALIANT_MICHELLE\EXPORT\MASTERDASH4.xlsx"
-kpi_file_location = r"C:\Users\user\Downloads\Alliant Total Org Data 2023-2025.xlsx"
+file_location = r"MASTERDASH4.xlsx"
+kpi_file_location = r"Alliant Total Org Data 2023-2025.xlsx"
 logo_path = "logo.jpeg"
 
 df = load_data(file_location)
@@ -735,3 +735,4 @@ if df is not None:
 
 else:
     st.warning("Data could not be loaded. Please check the file path and format.")
+
